14bebd5550f5f383d0f08d326a80dc385e39fe84
roopkt <roopkt@gmail.com>
2017-06-20 18:51:15 -0400

    debugging nested node

--- src/app/tree/tree-node.component.ts
+++ src/app/tree/tree-node.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
@@ -7,17 +7,21 @@ import 'rxjs/add/observable/of';
     template: `
     <div *ngIf="node">
     {{node.title}}
-      <app-tree-nodes [nodes$]="nodes$" *ngIf="hasChildren"></app-tree-nodes>
+      <app-tree-nodes [nodes$]="treeNodes$|async" *ngIf="hasChildren"></app-tree-nodes>
     </div>
   `
 })
 
-export class TreeNodeComponent {
+export class TreeNodeComponent implements OnInit {
     @Input() node;
+    ngOnInit() {
+        console.log(`initializing ${this.node.title}`, this.node);
+    }
     get treeNodes() {
         return Observable.of(this.node.nodes);
     }
     get hasChildren() {
+        console.log(`${this.node.title} hasChildren: `, this.node.nodes.length > 0);
         return this.node.nodes.length > 0;
     }
 }

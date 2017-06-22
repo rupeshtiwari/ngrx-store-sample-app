455dc625df652e76b70bff6b7595e942dc9b2409
roopkt <roopkt@gmail.com>
2017-06-22 16:02:28 -0400

    Updates

--- src/app/actions/todo.ts
+++
@@ -1,7 +0,0 @@
-export const ADD_TODO = '[Todo] Add';
-export const LOAD_TODO = '[Todo] load';
-export const TOGGLE_TODO = '[Todo] Toggle';
-export const addTodo = (todo) => { return { 'type': ADD_TODO, 'payload': todo }; };
-export const toggleTodo = (path) => { return { 'type': TOGGLE_TODO, 'payload': path }; };
-export const loadTodos = () => { return { 'type': LOAD_TODO, 'payload': '' }; };
-
--- src/app/actions/tree.ts
+++
@@ -1,5 +0,0 @@
-export const TOGGLE_NODE = '[Tree] toggle node';
-export const LOAD = '[Tree] load';
-export const toggleNode = (path) => { return { 'type': TOGGLE_NODE, 'payload': path }; };
-export const loadTree = () => { return { 'type': LOAD, 'payload': '' }; };
-

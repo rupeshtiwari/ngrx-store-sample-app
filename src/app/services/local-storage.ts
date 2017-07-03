a340e6255c728ee70a11f046f360502b6baa0741
roopkt <roopkt@gmail.com>
2017-07-03 11:47:29 -0400

    updated local storage

--- src/app/services/local-storage.ts
+++ src/app/services/local-storage.ts
@@ -4,10 +4,12 @@ import { tap } from 'ramda';
 import { initialAppState } from '../reducers/app-state';
 
 
-export const getKey = () => 'ngrx-app-key';
+export function getKey() { return 'ngrx-app-key'; }
 export const save = (k, v) => of(localStorage.setItem(k, JSON.stringify(v)));
 
-export const getState = (k) => JSON.parse(localStorage.getItem(k) || null) || initialAppState;
+export function getState(k) {
+  return JSON.parse(localStorage.getItem(k) || null) || initialAppState;
+}
 
-export const getAppState = () => getState(getKey());
+export function getAppState() { return getState(getKey()); }
 export const saveAppState = (state) => save(getKey(), state);
